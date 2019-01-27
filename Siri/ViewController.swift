d3fc744cd5300896f8cce2c7d07b467b29f4fcf2
Sixing Zheng (Skyler) <skylerzheng45@gmail.com>
2019-01-27 03:51:38 -0600

    post method

--- Siri/ViewController.swift
+++ Siri/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
     
     // add more code
+    let URL_SAVE_TEAM = "http://192.168.1.103/MyWebService/api/createteam.php"
+    
     private var finalInformationList = ["","","","","","","",""];
     public var tempInformationList = ["","","","","","","",""];
     @IBOutlet weak var startingLocationTextField: UITextField!
@@ -69,6 +71,47 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             audioEngine.stop()
             // text that will be uploaded
             print(textView.text)
+            // server
+            //created NSURL
+            let requestURL = NSURL(string: URL_SAVE_TEAM)
+            //creating NSMutableURLRequest
+            let request = NSMutableURLRequest(url: requestURL! as URL)
+            //setting the method to post
+            request.httpMethod = "POST"
+            //getting values from text fields
+            let teamName = textView.text
+            let memberCount = "Nothing here"
+            //creating the post parameter by concatenating the keys and values from text field
+            let postParameters = "name="+teamName!+"&member="+memberCount;
+            //adding the parameters to request body
+            request.httpBody = postParameters.data(using: String.Encoding.utf8)
+            //creating a task to send the post request
+            let task = URLSession.shared.dataTask(with: request as URLRequest){
+                data, response, error in
+                if error != nil{
+                    print("error is \(error)")
+                    return;
+                }
+                //parsing the response
+                do {
+                    //converting resonse to NSDictionary
+                    let myJSON =  try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                    //parsing the json
+                    if let parseJSON = myJSON {
+                        //creating a string
+                        var msg : String!
+                        //getting the json response
+                        msg = parseJSON["message"] as! String?
+                        //printing the response
+                        print(msg)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+            //executing the task
+            task.resume()
+            // server
             
             recognitionRequest?.endAudio()
             microphoneButton.isEnabled = false
