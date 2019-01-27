99691d271783d94d89f9eec971f60349bb5f641f
Abinav Chandar <abinavchandar@gmail.com>
2019-01-27 00:18:45 -0600

    Add files via upload

--- json_mysql.py
+++ json_mysql.py
@@ -2,14 +2,28 @@ import json
 from pprint import pprint
 import mysql.connector
 
-departureTime_hour = []
-departureTime_minute = []
-flightNumber = []
-aircraftType = []
-destination = []
-flightStatus = []
-baseCost = []
-durationInMinutes = []
+import MySQLdb
+ 
+connection = mysql.connector.connect(host='localhost',
+                             database='easyfly',
+                             user='root',
+                             password='abinav2019$')
+ 
+
+
+departureTime_hour = ""
+departureTime_minute = ""
+flightNumber = ""
+aircraftType = ""
+destination = ""
+flightStatus = ""
+baseCost = ""
+durationInMinutes = ""
+fl = "lo"
+new_val = ""
+
+
+ 
 
 
 
@@ -20,19 +34,41 @@ with open('/Users/abinavc/AA-Mock-Engine/mock/flightData.json') as f:
 
     	for x in i["flights"]:
 
-    		flightNumber.append(x["flightNumber"])
-    		aircraftType.append(x["aircraftType"])
-    		destination.append(x["destination"])
-    		flightStatus.append(x["flightStatus"])
-    		baseCost.append(x["baseCost"])
-    		durationInMinutes.append(x["durationInMinutes"])
-    		departureTime_hour.append(x["departureTime"]["hour"])
-    		departureTime_minute.append(x["departureTime"]["minute"])
+    		flightNumber = str(json.dumps((x["flightNumber"])))
+    		aircraftType = str(json.dumps((x["aircraftType"])))
+    		destination = str(json.dumps((x["destination"])))
+    		flightStatus = str(json.dumps((x["flightStatus"])))
+    		baseCost = str(json.dumps((x["baseCost"])))
+    		durationInMinutes = str(json.dumps((x["durationInMinutes"])))
+    		departureTime_hour = str(json.dumps((x["departureTime"]["hour"])))
+    		departureTime_minute = str(json.dumps((x["departureTime"]["minute"])))
+    		new_val = str(json.dumps(aircraftType))
+    		cursor = connection.cursor()
+    		sql_insert_query = cursor.execute("INSERT INTO flight_data (flight_number, aircraft_type, destination, flight_status, base_cost, duration_minutes, departuretime_hour, departuretime_minute) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s)", (flightNumber, aircraftType, destination, flightStatus, baseCost, durationInMinutes, departureTime_hour, departureTime_minute));
 
+    		
+    		connection.commit()
+
+
+
+                   
+
+            			  
+           
+
+         				  
+
+    		
+
+    		#sql = "INSERT INTO flight_data (id, flight_number, aircraft_type, destination, flight_status, base_cost, duration_minutes, departuretime_hour, departuretime_minute, departuretime_minute) VALUES ('', flightNumber, aircraftType, destination, flightStatus, baseCost, durationInMinutes, departureTime_hour, departureTime_minute)"
+    		#mycursor.execute(sql)
+    		#mydb.commit()
+    		#print(mycursor.rowcount, "record inserted.")
     		#print(x["destination"])
   
     			#departureTime_minute.append(y["minute"])
     			#print()
+
 
 
 
